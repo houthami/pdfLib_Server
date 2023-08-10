e042a3b033216c9feb22bca18499a07b66e08ca3
Ismael Ait El Kamel <ismael.aitelkamel17@gmail.com>
2023-08-10 20:16:06 +0100

    Complited the creation of PDF to Images conversion API.

--- libraries.py
+++ libraries.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
-    
-
+from pdf2image import convert_from_bytes
+import os
+from starlette.middleware.cors import CORSMiddleware
--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-
 from libraries import *
 app = FastAPI()
 
-@app.get("/")
-def welcome():
-    return {"message": 'api Hello world '}
+# @app.get("/")
+# def welcome():
+#     return {"message": 'api Hello world '}
+
+
+origins = ["*"]
+app.add_middleware(CORSMiddleware, allow_origins=origins,
+                   allow_methods=["*"], allow_headers=["*"])
+
+
+@app.post('/conversion')
+async def Pdf_to_image(file: UploadFile = File(...)):
+    try:
+        pdf_bytes = await file.read()
+        images = convert_from_bytes(pdf_bytes)
+        image_urls = saveImage(images)
+        return {"url": image_urls}
+    except Exception as e:
+        return {"error": str(e), "type": type(e).__name__}
+
+# save the images to desktop and return their urls
+
+
+def saveImage(image):
+    image_urls = []
+    for i, image in enumerate(image):
+        image_path = f'image_{i}.png'
+        # Create the static folder (dir) if it does not exist
+        if not os.path.exists('static'):
+            os.mkdir('static')
+        image.save(f'static/{image_path}', "PNG")
+        image_urls.append(image_path)
+    return image_urls
