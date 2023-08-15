c7a9c6d3cbccc2e5c7f0cb6f4a5b6f1ca9692395
Hassan Outhami <houthami@orsys.co.ma>
2023-08-15 14:36:37 +0100

    CR.Fix base64

--- libraries.py
+++ libraries.py
@@ -1,7 +1,10 @@
 
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
+from PIL  import Image
+import io
 from pdf2image import convert_from_bytes
 import os
 from starlette.middleware.cors import CORSMiddleware
 import base64
+poppler_path=r'poppler-0.68.0/bin'
--- main.py
+++ main.py
@@ -15,15 +15,25 @@ app.add_middleware(CORSMiddleware, allow_origins=origins,
 async def pdf_to_base64Image(file: UploadFile = File(...)):
     try:
         pdf_bytes = await file.read()
-        images = convert_from_bytes(pdf_bytes)
-        base64_images = convert_to_base64(images)
-        return {"Pages": base64_images}
+
+        images = convert_from_bytes(pdf_bytes, poppler_path=poppler_path)
+
+        base64_images = []
+        for i, image in enumerate(images):
+            img_byte_array = io.BytesIO()
+            image.save(img_byte_array, format='PNG')  # You can choose other image formats if needed
+            img_byte_array = img_byte_array.getvalue()
+            base64_image = base64.b64encode(img_byte_array).decode('utf-8')
+            base64_images.append({"page": i + 1, "base64": base64_image})
+
+        return {"pages": base64_images}
     except Exception as e:
-        return {"error": str(e), "type": type(e).__name__}
+        return {"error": str(e)}
 
 
 def convert_to_base64(images):
     base64_images = []
+    print(type(images))
     for i, image in enumerate(images):
         img_data = image_to_base64(image)
         base64_images.append({"page": i+1, "base64": img_data})
