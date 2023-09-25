bbb6495db16f6e31802387468a020f7d921fcda8
ismaelaek <ismael.aitelkamel17@gmail.com>
2023-09-25 15:13:02 +0100

    API Update

--- libraries.py
+++ libraries.py
@@ -1,10 +1,10 @@
 
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
-from PIL  import Image
+from PIL import Image
 import io
 from pdf2image import convert_from_bytes
 import os
 from starlette.middleware.cors import CORSMiddleware
 import base64
-poppler_path=r'poppler-0.68.0/bin'
+poppler_path = r'poppler-23.08.0/Library/bin'
--- main.py
+++ main.py
@@ -21,10 +21,16 @@ async def pdf_to_base64Image(file: UploadFile = File(...)):
         base64_images = []
         for i, image in enumerate(images):
             img_byte_array = io.BytesIO()
-            image.save(img_byte_array, format='PNG')  # You can choose other image formats if needed
+            image.save(img_byte_array, format='PNG')
             img_byte_array = img_byte_array.getvalue()
             base64_image = base64.b64encode(img_byte_array).decode('utf-8')
-            base64_images.append({"page": i + 1, "base64": base64_image})
+            width, height = image.size
+            base64_images.append({
+                "page": i + 1,
+                "base64": base64_image,
+                "width": width,
+                "height": height
+            })
 
         return {"pages": base64_images}
     except Exception as e:
@@ -44,38 +50,3 @@ def image_to_base64(image):
     img_byte_array = image.convert('RGB').tobytes()
     base64_data = base64.b64encode(img_byte_array).decode('utf-8')
     return base64_data
-
-# @app.post('/conversion')
-# async def Pdf_to_image(file: UploadFile = File(...)):
-#     try:
-#         pdf_bytes = await file.read()
-#         images = convert_from_bytes(pdf_bytes)
-#         image_urls = saveImage(images)
-#         return {"url": image_urls}
-#     except Exception as e:
-#         return {"error": str(e), "type": type(e).__name__}
-
-# # save the images to desktop and return their urls
-
-
-# def saveImage(image):
-#     image_urls = []
-#     for i, image in enumerate(image):
-#         image_path = f'image_{i}.png'
-#         # Create the static folder (dir) if it does not exist
-#         if not os.path.exists('static'):
-#             os.mkdir('static')
-#         image.save(f'static/{image_path}', "PNG")
-#         image_urls.append(image_path)
-#     return image_urls
-
-######## This takes saved image from local storage and encode them to base64 #####
-# @app.post('/converted_images_base64')
-# def get_converted_images_base64():
-#     base64_images = []
-#     for i, image_path in enumerate(os.listdir('static')):
-#         with open(os.path.join('static', image_path), "rb") as image_file:
-#             image_data = image_file.read()
-#             base64_image = base64.b64encode(image_data).decode('utf-8')
-#             base64_images.append({"page": i+1, "base64": base64_image})
-#     return base64_images
